f5bba51467d8209f359ce28b5e065434da76b3b4
Psyanide <martin19957777@gmail.com>
2026-01-11 13:18:17 +0100

    feat: Add route guards for authentication and enhance login styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,27 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { RegisterComponent } from './components/register/register';
 import { DashboardComponent } from './components/dashboard/dashboard';
+import { authGuard, publicGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    canActivate: [publicGuard]
   },
   {
     path: 'register',
-    component: RegisterComponent
+    component: RegisterComponent,
+    canActivate: [publicGuard]
   },
   {
     path: 'dashboard',
-    component: DashboardComponent
+    component: DashboardComponent,
+    canActivate: [authGuard]
   },
   {
     path: '',
-    redirectTo: '/login',
+    redirectTo: '/dashboard',
     pathMatch: 'full'
   }
 ];
